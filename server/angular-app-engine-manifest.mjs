5b47e2e4b185ed65a84b6c73f12a957f73a33652
kashyap369 <shubahm983346@gmai.com>
2025-07-24 16:06:31 +0530

    Auto-generated commit

--- server/angular-app-engine-manifest.mjs
+++ server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/TaskFlow',
+  basePath: '/',
   supportedLocales: {
   "en-US": ""
 },
--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -2,12 +2,12 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/TaskFlow/',
+  baseHref: '/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/TaskFlow"
+    "route": "/"
   },
   {
     "renderMode": 2,
@@ -15,35 +15,35 @@ export default {
       "chunk-S4LQEZFE.js",
       "chunk-YECR6WQL.js"
     ],
-    "route": "/TaskFlow/dashboard"
+    "route": "/dashboard"
   },
   {
     "renderMode": 2,
     "preload": [
       "chunk-L6P3XZSA.js"
     ],
-    "route": "/TaskFlow/tasks"
+    "route": "/tasks"
   },
   {
     "renderMode": 2,
     "preload": [
       "chunk-GNFGXKIJ.js"
     ],
-    "route": "/TaskFlow/projects"
+    "route": "/projects"
   },
   {
     "renderMode": 2,
     "preload": [
       "chunk-XVAXMSVJ.js"
     ],
-    "route": "/TaskFlow/team"
+    "route": "/team"
   },
   {
     "renderMode": 2,
     "preload": [
       "chunk-TWS23EC7.js"
     ],
-    "route": "/TaskFlow/calendar"
+    "route": "/calendar"
   },
   {
     "renderMode": 2,
@@ -51,36 +51,36 @@ export default {
       "chunk-H2YQHVCO.js",
       "chunk-YECR6WQL.js"
     ],
-    "route": "/TaskFlow/settings"
+    "route": "/settings"
   },
   {
     "renderMode": 2,
     "preload": [
       "chunk-KQDXCQWK.js"
     ],
-    "route": "/TaskFlow/reports"
+    "route": "/reports"
   },
   {
     "renderMode": 2,
     "preload": [
       "chunk-TQCDD77H.js"
     ],
-    "route": "/TaskFlow/time-tracking"
+    "route": "/time-tracking"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 5129, hash: '08cb7dc2b84962973840df0e732d13e4b1e97cac7223e0c11ddefe7923050218', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1058, hash: 'daa50f21ad6b2b1d070f4424cb15c186e2ce23e21e4393ce6d6c21695bf630b5', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 5120, hash: '95e8b1f45072cf0dcd64eb225314d4189c982fc4839d548ac83f5bc681b96393', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1049, hash: 'a547ee778bf69325f2b9be9a104ba9e56dd7c5fe1011e3c2160b1c0486b51504', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'index.html': {size: 0, hash: 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'tasks/index.html': {size: 42101, hash: '8e2295723342a8ff5edfd9813c16467b2831094e5a3008e11b587684079d0686', text: () => import('./assets-chunks/tasks_index_html.mjs').then(m => m.default)},
-    'calendar/index.html': {size: 39574, hash: '0855e2d642f6ddae54110af463610e67b45efa7255e76461c297a8d08e0f59ba', text: () => import('./assets-chunks/calendar_index_html.mjs').then(m => m.default)},
-    'reports/index.html': {size: 52242, hash: 'd50d292f5343b57e5178d6f4121fe60404935be80e16e6343c74fc9e419cbae1', text: () => import('./assets-chunks/reports_index_html.mjs').then(m => m.default)},
-    'settings/index.html': {size: 39511, hash: '21594866ff8106b5710d8c0b3114c011316ab0fb1058dc56d1d3a089294eff45', text: () => import('./assets-chunks/settings_index_html.mjs').then(m => m.default)},
-    'dashboard/index.html': {size: 47809, hash: '3b6777f2a56a1745b219f9a50215f654ec18bb589ff17152b1bdf8993591df81', text: () => import('./assets-chunks/dashboard_index_html.mjs').then(m => m.default)},
-    'time-tracking/index.html': {size: 45164, hash: '6435dd8fc600d50485b68ecfc24a798209bf7aba7dbe0265c21b89ef489b0038', text: () => import('./assets-chunks/time-tracking_index_html.mjs').then(m => m.default)},
-    'projects/index.html': {size: 60610, hash: '9f9222f8e8bdba6e641a5d4f077757f5fc08a08386c3a1da668c2d66628899c6', text: () => import('./assets-chunks/projects_index_html.mjs').then(m => m.default)},
-    'team/index.html': {size: 78983, hash: '954e908ee1cb6e18c1cd395ac6880e2e1c5cad48249cbb5133e9823fb40aa920', text: () => import('./assets-chunks/team_index_html.mjs').then(m => m.default)},
+    'settings/index.html': {size: 39430, hash: 'fd2c801c3c90a4ace4ba0ff3c82a94e2e988828545fe27a8ae0370489c2e8763', text: () => import('./assets-chunks/settings_index_html.mjs').then(m => m.default)},
+    'dashboard/index.html': {size: 47728, hash: '40ac456342fbc4a3674b63a6fe95191da8f817acc3ceb7564522a7105629d45c', text: () => import('./assets-chunks/dashboard_index_html.mjs').then(m => m.default)},
+    'calendar/index.html': {size: 39493, hash: 'bae094428bfadeac6ccc293f5b2cadc5deee16adb0bbc6cec73941b6065903cd', text: () => import('./assets-chunks/calendar_index_html.mjs').then(m => m.default)},
+    'reports/index.html': {size: 52165, hash: '18910dd39745926f5bf9b26d6fc29adcd389895b2004ce3cdc5716ec98212f02', text: () => import('./assets-chunks/reports_index_html.mjs').then(m => m.default)},
+    'projects/index.html': {size: 60529, hash: 'ae22b3ab518d90e8264dce19cbb55899b4e2106e3dd77c94731cff07b61664d4', text: () => import('./assets-chunks/projects_index_html.mjs').then(m => m.default)},
+    'tasks/index.html': {size: 42020, hash: '4aeb62da6740e767b1cb39a8be1fd366a4bbc0c6950603318c8e6eadefa903ec', text: () => import('./assets-chunks/tasks_index_html.mjs').then(m => m.default)},
+    'time-tracking/index.html': {size: 45083, hash: '61c9c72de836d95679ef631fa7c0ee4fc910b44aeff65df6f0c1ea5e3ac0e32c', text: () => import('./assets-chunks/time-tracking_index_html.mjs').then(m => m.default)},
+    'team/index.html': {size: 78908, hash: '9345517632aff683f953309f48facdf67e7c907a4f46bb54b9879d5ded91d2fb', text: () => import('./assets-chunks/team_index_html.mjs').then(m => m.default)},
     'styles-3FDS4ZVF.css': {size: 230916, hash: '635mquZSb2k', text: () => import('./assets-chunks/styles-3FDS4ZVF_css.mjs').then(m => m.default)}
   },
 };
